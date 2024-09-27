86fe4dab010cc40db77111bdc9b2202ba56dc6ef
Fdev2be <saleh.touil@icloud.com>
2024-09-27 14:23:52 +0100

    fed data

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,15 +5,166 @@ data = {
     "Courbe 1": {
         "Ant1": "Wifi",
         "Ant2": "Dipole",
-        "Fe": "500 MHz",
-        "Pe": "4 dBm",
+        "Freq cte": "500 MHz",
+        "PuissanceEntree": "4 dBm",
         "data": {
-            "Distance (cm)": [100, 80, 60, 40, 20],
-            "Puissance recue (dBm)": [-59, -47, -39, -43, -39]
+            "Distance (cm)": [100, 80, 60],
+            "Puissance recue (dBm)": [-59, -47, -39]
         }
     },
-    # ... Ajoutez d'autres courbes ici ...
+    "Courbe 2": {
+        "Ant1": "Thoraya",
+        "Ant2": "Dipole",
+        "Freq cte": "500 MHz",
+        "Distance": "1 m",
+        "data": {
+            "Puissance (dBm)": [2, 4, 6, 8, 10],
+            "Puissance recue (dBm)": [-67, -58, -52, -46, -35]
+        }
+    },
+    "Courbe 3": {
+        "Ant1": "Dipole",
+        "Ant2": "Dipole",
+        "Freq": "550 MHz",
+        "Distance": "1 m",
+        "data": {
+            "Puissance (dBm)": [4, 6, 8, 10],
+            "Puissance recue (dBm)": [-52, -43, -38, -31]
+        }
+    },
+    "Courbe 4": {
+        "Ant1": "Dipole",
+        "Ant2": "Dipole",
+        "Freq cte": "550 MHz",
+        "Distance": "1 m",
+        "data": {
+            "Puissance (dBm)": [4, 6, 8, 10],
+            "Puissance recue (dBm)": [-52, -43, -38, -31]
+        }
+    },
+    "Courbe 5": {
+        "Ant1": "Dipole", 
+        "Ant2": "Dipole",
+        "Freq cte" : "550 MHz",
+        "Distance cte": "1m",
+        "data": {
+             "PuissanceEntree (dBm)": [4,6,8,10],
+             "Puissance recue (dBm)": [-52, -43, -38, -31]
+        }
+    },
+    "Courbe 6": {
+        "Ant1": "Dipole", 
+        "Ant2": "Dipole",
+        "Puissance cte": "4 dBm",
+        "Distance cte": "1m",
+        "data": {
+             "Freq (MHz)": [550, 650, 750, 850],
+             "Puissance recue (dBm)": [-33, -38, -39, -35]
+        }
+    },
+    "Courbe 7": {
+        "Ant1": "Dipole", 
+        "Ant2": "Dipole",
+        "Puissance cte": "4 dBm",
+        "Freq cte" : "650 MHz",
+        "data": {
+             "Distance (m)": [1, 0.8, 0.6, 0.4, 0.2],
+             "Puissance recue (dBm)": [-43, -35, -25, -20, -18]
+        }
+    },
+    "Courbe 8": {
+        "Ant1": "Yagi", 
+        "Ant2": "Dipole",
+        "Puissance cte": "4 dBm",
+        "Distance cte" : "1m",
+        "data": {
+             "Freq (MHz)": [750, 850, 950],
+             "Puissance recue (dBm)": [-45, -38, -34]
+        }
+    },
+    "Courbe 9": {
+        "Ant1": "Yagi", 
+        "Ant2": "Dipole",
+        "Puissance cte": "0 dBm",
+        "Distance cte" : "1m",
+        "data": {
+             "Freq (MHz)": [550, 650, 750, 850, 950],
+             "Puissance recue (dBm)": [-48, -43, -40, -42, -35]
+        }
+    },
+    "Courbe 10": {
+        "Ant1": "Yagi", 
+        "Ant2": "Dipole",
+        "Puissance cte": "0 dBm",
+        "Freq cte" : "550 MHz",
+        "data": {
+             "Distance (m)": [1, 0.8, 0.6, 0.4],
+             "Puissance recue (dBm)": [-66, -50, -43, -35]
+        }
+    },
+    "Courbe 11": {
+        "Ant1": "Helical", 
+        "Ant2": "Loop",
+        "Puissance cte": "0 dBm",
+        "Freq cte" : "6 GHz",
+        "data": {
+             "Puissance (dBm)": [10,12],
+             "Puissance recue (dBm)": [-43, -37]
+        }
+    },
+    "Courbe 12": {
+        "Ant1": "Helical", 
+        "Ant2": "Loop",
+        "Puissance cte": "6 dBm",
+        "Freq cte" : "1.6 GHz",
+        "data": {
+             "Distance (m)": [1, 0.8, 0.6, 0.4, 0.2],
+             "Puissance recue (dBm)": [-52, -54, -47, -29, -29]
+        }
+    },
+    "Courbe 13": {
+        "Ant1": "Hauraya", 
+        "Ant2": "Loop",
+        "Puissance cte": "0 dBm",
+        "Distance cte" : "1m",
+        "data": {
+             "Freq (MHz)": [550, 650, 750, 850, 950],
+             "Puissance recue (dBm)": [-60, -48, -69, -71, -43]
+        }
+    },
+    "Courbe 14": {
+        "Ant1": "Hauraya", 
+        "Ant2": "Loop",
+        "Puissance cte": "4 dBm",
+        "Distance cte" : "1m",
+        "data": {
+             "Puissance (dBm)": [2, 4, 6, 8, 10],
+             "Puissance recue (dBm)": [-60, -47, -40, -38, -35]
+        }
+    },
+    "Courbe 15": {
+        "Ant1": "Hauraya", 
+        "Ant2": "Loop",
+        "Puissance cte": "4 dBm",
+        "Distance cte" : "1m",
+        "data": {
+             "Freq (MHz)": [100, 200, 300, 400, 500],
+             "Puissance recue (dBm)": [-37, -36, -35, -37, -33]
+        }
+    },
+    "Courbe 16": {
+        "Ant1": "Hauraya", 
+        "Ant2": "Loop",
+        "Puissance cte": "4 dBm",
+        "Freq cte" : "300 MHz",
+        "data": {
+             "Distance (m)": [1, 0.8, 0.6, 0.4, 0.2],
+             "Puissance recue (dBm)": [-43, -35, -39, -34, -29]
+        }
+    }
 }
+
+# ... (Le reste du code est identique au code précédent) ...
 
 st.title("Visualisation des données")
 
