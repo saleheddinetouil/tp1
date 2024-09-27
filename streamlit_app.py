7e5ca5f8d83c4ca45c72fffd0a0d1f10888d7bfb
Fdev2be <saleh.touil@icloud.com>
2024-09-27 13:45:05 +0100

    Refactor data visualization and add scatter plot markers with labels in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ for courbe_name, courbe_data in data.items():
     x_data = list(courbe_data["data"].values())[0]
     y_data = list(courbe_data["data"].values())[1]
     # display under figure
-    ax.scatter(x_data, y_data, label=legend_label, marker="o")
+    ax.scatter(x_data, y_data, label=legend_label, marker="o", color="blue",x="Distance (cm)",y="Puissance recue (dBm)")
 
 
 
